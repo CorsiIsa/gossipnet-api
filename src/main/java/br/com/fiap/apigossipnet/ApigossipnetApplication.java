1173124d7146164c3f4fa0f54ac536f51187dc9a
CorsiIsa <icorsi15@gmail.com>
2025-04-06 16:04:18 -0300

    first commit

---
+++ src/main/java/br/com/fiap/apigossipnet/ApigossipnetApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apigossipnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigossipnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigossipnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/apigossipnet/ApigossipnetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apigossipnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigossipnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
